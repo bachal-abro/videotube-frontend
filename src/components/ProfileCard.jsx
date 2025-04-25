ee56f126220c84207f29051e16c9c7fdd9dbeab6
Muhammad Bachal <mbachalabro1@gmail.com>
2025-04-26 01:29:39 +0500

    Integrate user fetching in ProfileCard component; replace video owner data with API call to retrieve user details

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useGetUserByIdQuery } from "../features/users/usersApiSlice";
+useGetUserByIdQuery;
 
-const ProfileCard = ({ video}) => {
-  // const { video } = useSelector((store) => store.videoView);
-  const creator = video?.owner
-  
-  return (
-    <div className="flex items-center gap-4">
-      <img
-        className="h-10 w-10 mt-1 rounded-full sm:block"
-        src={creator?.avatar}
-        alt="Avatar"
-      />
-      <div className="flex flex-col">
-        <h1 className="font-medium">{creator?.fullName}</h1>
-        <p className="text-sm text-gray-300">{creator?.subscribers} Subscribers</p>
-      </div>
-    </div>
-  );
+const ProfileCard = ({ video }) => {
+    // const { video } = useSelector((store) => store.videoView);
+    const ownerId = video?.owner;
+    const { data, isLoading, isSuccess, isError, error } =
+        useGetUserByIdQuery(ownerId);
+
+    if (isLoading) {
+        return <p>"Loading..."</p>;
+    } else if (isSuccess) {
+        const creator = data.data;
+
+        return (
+            <div className="flex items-center gap-4">
+                <img
+                    className="h-10 w-10 mt-1 rounded-full sm:block"
+                    src={creator?.avatar}
+                    alt="Avatar"
+                />
+                <div className="flex flex-col">
+                    <h1 className="font-medium">{creator?.fullName}</h1>
+                    {/* <p className="text-sm text-gray-300">{creator?.subscribers} Subscribers</p> */}
+                </div>
+            </div>
+        );
+    }
 };
 
 export default ProfileCard;
--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -3,12 +3,17 @@ import { apiSlice } from "../../app/api/apiSlice"
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
         getUsers: builder.query({
-            query: () => '/current-user',
+            query: () => 'users/current-user',
             keepUnusedDataFor: 60,
-        })
+        }),
+        getUserById: builder.query({
+            query: (userId) => `users/${userId}`,
+            keepUnusedDataFor: 60,
+        }),
     })
 })
 
 export const {
-    useGetUsersQuery
+    useGetUsersQuery,
+    useGetUserByIdQuery
 } = usersApiSlice 
